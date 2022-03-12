9d20f8262ebc34c00a47c6a50e2b2fcb39912cb8
Tal Spektor <tal.s@zemingo.com>
2022-03-12 22:57:51 +0200

    Note validation

---
+++ lib/domain/notes/value_objects.dart
@@ -0,0 +1,87 @@
+import 'package:dartz/dartz.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_ddd/domain/core/failures.dart';
+import 'package:flutter_ddd/domain/core/value_objects.dart';
+import 'package:flutter_ddd/domain/core/value_transformers.dart';
+import 'package:kt_dart/kt.dart';
+
+import '../core/value_validators.dart';
+
+class NoteBody extends ValueObject<String> {
+  @override
+  final Either<ValueFailure<String>, String> value;
+
+  static const maxLength = 1000;
+
+  factory NoteBody(String input) {
+    return NoteBody._(
+      validateMaxStringLength(input, maxLength).flatMap(validateStringNotEmpty),
+    );
+  }
+
+  const NoteBody._(this.value);
+}
+
+class TodoName extends ValueObject<String> {
+  @override
+  final Either<ValueFailure<String>, String> value;
+
+  static const maxLength = 30;
+
+  factory TodoName(String input) {
+    return TodoName._(
+      validateMaxStringLength(input, maxLength)
+          .flatMap(validateStringNotEmpty)
+          .flatMap(validateSingleLine),
+    );
+  }
+
+  const TodoName._(this.value);
+}
+
+class NoteColor extends ValueObject<Color> {
+  static const List<Color> predefinedColors = [
+    Color(0xfffafafa),
+    Color(0xfffa8072),
+    Color(0xfffedc56),
+    Color(0xffd0f0c0),
+    Color(0xfffca3b7),
+    Color(0xff997950),
+    Color(0xfffffdd0),
+  ];
+
+  @override
+  final Either<ValueFailure<Color>, Color> value;
+
+  factory NoteColor(Color input) {
+    return NoteColor._(
+      right(makeColorOpaque(input)),
+    );
+  }
+
+  const NoteColor._(this.value);
+}
+
+class List3<T> extends ValueObject<KtList<T>> {
+  @override
+  final Either<ValueFailure<KtList<T>>, KtList<T>> value;
+
+  static const maxLength = 3;
+
+  factory List3(KtList<T> input) {
+    return List3._(
+      validateMaxListLength(input, maxLength),
+    );
+  }
+
+  const List3._(this.value);
+
+  int get length {
+    return value.getOrElse(() => emptyList()).size;
+  }
+
+  bool get isFull {
+    return length == maxLength;
+  }
+}
